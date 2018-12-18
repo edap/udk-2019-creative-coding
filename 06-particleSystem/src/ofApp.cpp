89c5b80e52a48ecef146226c52ab0b18c7ae1ab6
Davide Prati <lastexxit@gmail.com>
2018-12-18 16:58:24 +0100

    add particle system ex

---
+++ 06-particleSystem/src/ofApp.cpp
@@ -0,0 +1,109 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    reset();
+}
+
+//NEW
+void ofApp::reset(){
+    points.clear();
+    for(int i=0; i<nParticles; i++){
+        Snowflake s;
+        s.position  = glm::vec2(ofRandom(ofGetWidth()),
+                                ofRandom(ofGetHeight()));
+        ofColor col;
+        col.setHsb(ofRandom(100, 200), 255, 255);
+        s.color = col;
+        s.radius = ofRandom(10, 20);
+        s.drag = ofRandom(0.97, 0.99);
+        //the unique val allows us to set properties slightly differently for each particle
+        s.uniqueVal = ofRandom(-10000, 10000);
+
+        points.push_back(s);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    for(auto & s:points){
+        s.update(mode);
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackgroundGradient(ofColor(210,160,0), ofColor(10,200,0));
+    for(auto s:points){
+        s.draw(drawTrail);
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if( key == '1'){
+        reset();
+        mode = PARTICLE_MODE_ATTRACT;
+    }else if( key == '2'){
+        reset();
+        mode = PARTICLE_MODE_FALL;
+    }else if( key == '3'){
+        reset();
+        mode = PARTICLE_MODE_REPEL;
+    }else if('t'){
+        drawTrail = !drawTrail;
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ 06-particleSystem/src/ofApp.h
@@ -0,0 +1,178 @@
+#pragma once
+
+#include "ofMain.h"
+// reference, spiega che questo esercizio viene dagli esempi
+// 1 Aggiungi il methodo reset
+// muovi fall() into update
+// introduci enum, passalo come argomento per update
+// salto indietro alla posizione che cambia con la velocity
+// aggiungi l'attraction
+// aggiungi repel, soffermati sul cambio di direzione rispetto a attract
+// spiega signed noise, e perche' unique Val
+// Fai vedere cosa succede se usi repel, le particelle escono dallo schermo
+// tienile nello schermo
+// Aggiungi la trail. Come farebbero loro?
+
+// NEW
+enum particleMode{
+    PARTICLE_MODE_ATTRACT = 0,
+    PARTICLE_MODE_REPEL,
+    PARTICLE_MODE_FALL
+};
+
+class Snowflake{
+public:
+    Snowflake(){};
+
+    //Properties
+    glm::vec2 position;
+
+    // NEW
+    glm::vec2 velocity;
+    glm::vec2 force;
+    float drag;
+    float uniqueVal;
+    float scale;
+    vector<glm::vec2> trails;
+
+    float radius;
+    ofColor color;
+
+    //Methods
+    void fall(){
+        position.y+= 0.5;
+        if(position.y >= ofGetHeight()){
+            position.y = 0;
+            position.x = ofRandom(ofGetWidth());
+        }
+    }
+
+    //NEW
+    void attract(){
+        glm::vec2 attractPt(ofGetMouseX(), ofGetMouseY());
+        force = attractPt-position; // we get the attraction force/vector by looking at the mouse pos relative to our pos
+        force = glm::normalize(force); //by normalizing we disregard how close the particle is to the attraction point
+
+        velocity *= drag; //apply drag
+        velocity += force * 0.6; //apply force
+        position += velocity;
+    }
+
+    //NEW
+    void repel(){
+        glm::vec2 repelPt(ofGetMouseX(), ofGetMouseY());
+        force = repelPt-position;
+
+        //let get the distance and only repel points close to the mouse
+        float dist = glm::length(force);
+        force = glm::normalize(force);
+
+        velocity *= drag;
+        if( dist < 150 ){
+            velocity += -force * 0.6; //notice the force is negative
+        }else{
+            //if the particles are not close to us, lets add a little bit of random movement using noise. this is where uniqueVal comes in handy.
+            force.x = ofSignedNoise(uniqueVal, position.y * 0.01, ofGetElapsedTimef()*0.2);
+            force.y = ofSignedNoise(uniqueVal, position.x * 0.01, ofGetElapsedTimef()*0.2);
+            velocity += force * 0.04;
+        }
+        position+=velocity;
+    }
+
+    //NEW
+    void keepOnScreen(){
+        if( position.x > ofGetWidth() ){
+            position.x = ofGetWidth();
+            velocity.x *= -1.0;
+        }else if( position.x < 0 ){
+            position.x = 0;
+            velocity.x *= -1.0;
+        }
+        if( position.y > ofGetHeight() ){
+            position.y = ofGetHeight();
+            velocity.y *= -1.0;
+        }
+        else if( position.y < 0 ){
+            position.y = 0;
+            velocity.y *= -1.0;
+        }
+    }
+
+    void changeRadius(){
+        radius = ofMap(position.y, 0,ofGetHeight(), 10, 50);
+    }
+
+    void draw(bool trail = false){
+        ofPushStyle();
+        ofSetColor(color);
+        ofDrawCircle(position.x, position.y, radius);
+        ofPopStyle();
+
+        if (trail) {
+            int i = 0;
+            for(auto t:trails){
+                ofPushStyle();
+                ofSetColor(color);
+                ofDrawCircle(t.x, t.y, radius + i*2);
+                ofPopStyle();
+                i++;
+            }
+        }
+    }
+
+    //NEW
+    void update(particleMode mode){
+        //1 - APPLY THE FORCES BASED ON WHICH MODE WE ARE IN
+
+        if( mode == PARTICLE_MODE_ATTRACT ){
+            attract();
+        }else if(mode == PARTICLE_MODE_FALL){
+            fall();
+        }else if(mode == PARTICLE_MODE_REPEL){
+            repel();
+            keepOnScreen();
+        }
+        else{
+            //DEFAULT BEHAVIOUR
+            fall();
+        }
+
+        // update the trail
+        trails.push_back(position);
+        if(trails.size() > 10){
+            trails.erase(trails.begin());
+        }
+
+    }
+};
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+    vector<Snowflake> points;
+
+    //NEW
+    particleMode mode;
+    bool drawTrail = false;
+    void setMode(particleMode newMode);
+    void reset();
+    int nParticles = 200;
+		
+};
+
